77be207a910b4cba94b6e11ce4162bf4e5015b85
MieIlie <jasv003@gmail.com>
2026-07-13 10:30:14 +0700

    gateway-completion:increase maxMessageSize to 64KB for WebRTC SDP

--- services/gateway/internal/websocket/client.go
+++ services/gateway/internal/websocket/client.go
@@ -11,7 +11,7 @@ const (
 	writeWait      = 10 * time.Second
 	pongWait       = 60 * time.Second
 	pingPeriod     = (pongWait * 9) / 10
-	maxMessageSize = 1024
+	maxMessageSize = 65536
 )
 
 type Client struct {
